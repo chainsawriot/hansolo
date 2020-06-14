22ad9a70a5df8fbcc04713ef8a9b3b17087c82a4
chainsawriot <chainsawtiney@gmail.com>
2020-06-14 13:01:38 +0200

    Using shift and control

--- hansolo.js
+++ hansolo.js
@@ -1,10 +1,31 @@
-// 按 Esc 喊 Solo
-// 還應有 pass, draw 等
+// Press Shift to call "Solo"
+// Press left Ctrl - pass, draw, confirm
+// You need to manually press "play" or the card you want to play to prevent mistakes.
 
-document.onkeyup = function(e){
-    var e = e || window.event;
-    if(e.which == 27) {
-        document.querySelector("#bottom_button_call").click()
-      return false;
-    }
+function click_ignore(button_css) {
+  var element = document.querySelector(button_css)
+  if (element === null) {
+    // do nothing
+    return false
+  } else {
+    element.click()
+    return false
+  }
 }
+
+function forward () {
+  // click #button_confirm #button_draw #button_pass
+  // in this precedent order
+  ["#button_confirm", "#button_draw", "#button_pass"].forEach(button_css => click_ignore(button_css))
+}
+
+window.addEventListener("keydown", function(e) {
+  // space and arrow keys
+  if([16].indexOf(e.keyCode) > -1) {
+    e.preventDefault()
+    document.querySelector("#bottom_button_call").click()
+  } else if ([17].indexOf(e.keyCode) > -1) {
+    e.preventDefault()
+    forward()
+  }
+}, false);
